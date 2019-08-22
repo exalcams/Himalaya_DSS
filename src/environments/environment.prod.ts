6a88b487e25571d2b644425d30c7a724663a315e
exalcams <exalca.plant1@gmail.com>
2019-08-22 14:36:48 +0530

    Solved event parameter error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
     hmr       : false,
-    // baseAddress : 'http://192.168.1.11/digitalsignatureService/',
+    baseAddress : 'http://192.168.1.11/digitalsignatureService/',
     // baseAddress : 'http://192.168.5.107/digitalsignatureService/',
-    baseAddress : 'https://himapps.himalayawellness.com/digitalsignatureService/',
+    // baseAddress : 'https://himapps.himalayawellness.com/digitalsignatureService/',
     clientId : 'ngAuthApp'
 };
